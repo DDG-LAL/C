ba4c9b9253fda98d27cf4841ac65de422bf03eb7
DDG_LAL <1935088714@qq.com>
2022-04-23 01:32:47 +0800

    project1、2 增加注释

--- Project1_NumGuessingGame/Project1_NumGuessingGame/NumGuessingGame.c
+++ Project1_NumGuessingGame/Project1_NumGuessingGame/NumGuessingGame.c
@@ -1,13 +1,15 @@
+//猜数字游戏
+
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include<stdio.h>
 #include<time.h>
 #include<string.h>
 #include<stdlib.h>
-#include<Windows.h>
+#include<windows.h>
 
 
-void menu()	//菜单
+void menu()	 //菜单
 {
 	printf("**************************\n\n");
 	printf("          猜数字          \n\n");
@@ -16,16 +18,18 @@ void menu()	//菜单
 	printf("**************************\n\n");
 }
 
-int game()
+int game()  //猜数字
 {
-	system("cls");
-	int ret = rand() % 200 + 1;  //生成1至200的随机数
+	system("cls");  //system()用于执行系统命令，cls是清屏操作，system函数需要stdlib.h
+	//生成1至200的随机数
+	//rand用于生成随机数，需要stdlib.h
+	int ret = rand() % 200 + 1;  //rand返回值是int类型，返回的是0至0x7fff中的随机整数(0x7fff==32767)
 
-	//printf("%d\n", ret);//测试用
+	//printf("%d\n", ret);  //测试用
 
-	int guess = 0, flag = 0, n = 0, n_MAX = 10;
+	int guess = 0, flag = 0, n = 0, n_MAX = 10;  //n_MAX是机会次数，理论上用二分法在8次之内能试出答案
 	printf("\n规则:在1至200的整数中猜1个数，有%d次机会\n\n", n_MAX);
-	system("pause");
+	system("pause");  //pause 是暂停操作，cmd窗口中会显示"按任意键继续"
 	while (1)
 	{
 		n++;
@@ -37,7 +41,7 @@ int game()
 		}
 		else
 		{
-			system("cls");  //清屏
+			system("cls");
 			printf("\n第%d次:请输入猜的数:", n);
 			scanf("%d", &guess);
 			if (guess == ret)
@@ -49,12 +53,12 @@ int game()
 			else if (guess >= 1 && guess <= 200)
 			{
 				flag = guess > ret ? 1 : 0;
-				switch (flag)  //用flag判断执行状态
+				switch (flag)  //用flag判断状态
 				{
 				case 1:
 				{
 					printf("\n猜大了\n\n");
-					Sleep(1200);
+					Sleep(1200);  //Sleep用于定时休眠，单位是ms，需要windows.h
 					break;
 				}
 				case 0:
@@ -85,12 +89,12 @@ int game()
 	return -1;
 }
 
-void str_output(char arr[][70]) //输出字符数组
+void str_output(char arr[][70])  //输出二维字符数组
 {
 	printf("%s%s%s%s%s", arr[0], arr[1], arr[2], arr[3], arr[4]);
 }
 
-void success()
+void success()  //成功之后输出"YOU WIN!"字符画
 {
 	system("cls");
 	int i = 0, j = 0, k = 0, m = 0;
@@ -99,16 +103,17 @@ void success()
 						 { "|                                                            |\n" },
 						 { "|                                                            |\n" },
 						 { "|                                                            |\n" } };
-	char arr2[5][70] = { { "| y   y    ooo    u   u      w   w   w   iii   nn    n    ** |\n" },  //you win
+	char arr2[5][70] = { { "| y   y    ooo    u   u      w   w   w   iii   nn    n    ** |\n" },  
 						 { "|  y y    o   o   u   u      w   w   w    i    n n   n    ** |\n" },
 						 { "|   y     o   o   u   u       w w w w     i    n  n  n    ** |\n" },
 						 { "|   y     o   o   u   u        w   w      i    n   n n       |\n" },
-						 { "|   y      ooo     uuu         w   w     iii   n    nn    ** |\n" } };
+						 { "|   y      ooo     uuu         w   w     iii   n    nn    ** |\n" } };  //you win
 	char arr3[5][70];
 	for (i = 0; i < 5; i++)
-		strcpy(arr3[i], arr1[0]);
+		strcpy(arr3[i], arr1[0]);  //strcpy就是string copy，将字符串赋值给字符数组，需要string.h
 	int len = strlen(arr1[0]) - 2;  //len=61
 	//字符画从左向右移动的效果
+	//每次将arr2中右边的j列赋给arr1，并j++、清屏、打印，进行i次，形成从左向右移动的效果
 	for (i = 0; i < len; i++)  //i:列数
 	{
 		m = len - 1;
@@ -116,11 +121,11 @@ void success()
 		{
 			for (k = 0; k < 5; k++)  //k:行数
 			{
-				arr1[k][j] = arr2[k][m];
+				arr1[k][j] = arr2[k][m];  
 			}
 			m--;
 		}
-		str_output(arr1);  //
+		str_output(arr1);  
 		Sleep(50);
 		system("cls");
 	}
@@ -143,21 +148,23 @@ void success()
 	system("cls");
 }
 
-void fail()
+void fail()  //失败后输出"You lose"字符串
 {
 	int i = 0, j = 0, k = 0;
 	system("cls");
 	Sleep(1000);
 	char arr1[] = "                          ";
 	char arr2[] = "You  lose         ";
-	int len1 = strlen(arr1);
+	int len1 = strlen(arr1);  //strlen就是string length，求字符串长度，需要string.h
 	int len2 = strlen(arr2);
 	printf("\n\n%s\n\n", arr1);
 	Sleep(200);
-	for (i = 0; i < len2; i++)
+	//字符串从右向左移动的效果
+	//每次将arr2中左边的j个字符赋给arr1，并j++、清屏、打印，进行len2次，形成从右向左移动的效果
+	for (i = 0; i < len2; i++)  //i:arr2的字符个数
 	{
 		k = 0;
-		for (j = len1 - i; j < len1; j++)
+		for (j = len1 - i; j < len1; j++)  //j:确定某个字符的位置后，遍历arr2中(从该字符至最后一个字符)的每个位置
 		{
 			arr1[j] = arr2[k++];
 		}
@@ -173,6 +180,10 @@ void fail()
 
 int main()
 {
+	//time函数用于获取时间戳，需要time.h，返回值是time_t类型(是一种整型)
+	//srand用于为rand函数设置随机数"种子"，srand函数需要stdlib.h，参数应为unsigned int类型
+	//在调用rand之前必须先调用一次srand，否则生成的随机数会不够随机
+	//多次调用srand也会导致不够随机
 	srand((unsigned int)time(NULL));  //随机数初始化，整个工程中只需要进行一次
 	int choice = 0, outcome = 0;
 	do
@@ -185,8 +196,7 @@ int main()
 		case 1:
 		{
 			outcome = game();
-			//printf("%d\n", outcome);//测试
-			if (outcome)
+			if (outcome)  //根据游戏结果决定输出
 				success();
 			else if (!outcome)
 				fail();
--- Project2_GaussMainElementEliminationMethod/Project2_GaussMainElementEliminationMethod/GaussMainElementEliminationMethod.c
+++ Project2_GaussMainElementEliminationMethod/Project2_GaussMainElementEliminationMethod/GaussMainElementEliminationMethod.c
@@ -7,32 +7,33 @@
 #define MAXSIZE 100
 
 
-void input(double a[MAXSIZE][MAXSIZE + 1], long n)  
+void input(double a[MAXSIZE][MAXSIZE + 1], long n)  //输入n阶线性方程组的增广矩阵
 {
 	long i, j;
 	for (i = 0; i < n; i++)
 		for (j = 0; j < n + 1; j++)
 			scanf("%lf", &a[i][j]);
 }
-void output(double x[MAXSIZE], long n)
+void output(double x[MAXSIZE], long n)  //输出方程组的解
 {
 	long k;
 	for (k = 1; k <= n; k++)
-		printf("x[%d]=%lf ", k, x[k - 1]);
+		printf("x[%ld]=%lf ", k, x[k - 1]);
+	printf("\n");
 }
 int main()
 {
-	double a[MAXSIZE][MAXSIZE + 1], x[MAXSIZE], s, max, temp;
+	double a[MAXSIZE][MAXSIZE + 1], x[MAXSIZE], max, temp;
 	long n, i, j, k, maxi;
 	printf("输入方程组阶数:"); 
 	scanf("%ld", &n);  //%ld对应long类型
 	printf("输入增广矩阵:\n");
 	input(a, n);  //数组名就是地址，属于传址调用
-	for (k = 0; k <= n - 2; k++)  //k遍历系数矩阵中的每一列
+	for (k = 0; k < n - 1; k++)  //k遍历系数矩阵中除了最后一列之外的每一列
 	{
 		max = a[k][k];  //max存放列主元
-		maxi = k;  //maxi用于存放列主元的下标
-		for (i = k + 1; i <= n - 1; i++)  //k确定后，i遍历某一列中的每一行
+		maxi = k;  //maxi存放列主元的下标
+		for (i = k + 1; i < n; i++)  //k确定某一列后，i遍历该列中从第k+2行开始往下的每一行
 		{
 			if (fabs(a[i][k]) > fabs(max))  //选取列主元 
 			{
@@ -42,32 +43,33 @@ int main()
 		}
 		if (max == 0) 
 			break;  //主元不能为0 
-		if (maxi != k)  //交换行 
-			for (j = k; j <= n; j++)
+		if (maxi != k)  //若列主元不在第k+1行，则对增广矩阵进行交换行，将主元换到第k+1行
+			for (i = k; i < n + 1; i++)  //i遍历增广矩阵从第k+1列开始往右的每一列
 			{
-				temp = a[k][j];
-				a[k][j] = a[maxi][j];
-				a[maxi][j] = temp;
+				temp = a[k][i];
+				a[k][i] = a[maxi][i];
+				a[maxi][i] = temp;
 			}
-		for (i = k + 1; i <= n - 1; i++)  //行运算，使系数矩阵变成上三角矩阵 
+		//行运算，使系数矩阵逐步变成上三角矩阵
+		for (i = k + 1; i < n; i++)  //i遍历增广矩阵中从第k+2行开始往下的每一行
 		{
-			a[i][k] /= -a[k][k];
-			for (j = k + 1; j <= n; j++) 
-				a[i][j] += a[i][k] * a[k][j];
+			temp = a[i][k] / -a[k][k];  //获得行运算的系数temp
+			for (j = k + 1; j < n + 1; j++)  //i确定某一行后，j遍历增广矩阵中从第k+2列开始往右的每一列
+				a[i][j] += temp * a[k][j];  //将temp乘以第k+1行，加到当前确定的行，使得该行第一个元素变为0
 		}
 	}
 	if (max == 0)  //主元不能为0 
 		printf("\n错误\n");
 	else
 	{
-		for (k = n - 1; k >= 0; k--)  //回代 
+		for (k = n - 1; k >= 0; k--)  //回代求解，k从增广矩阵的最后一行遍历到第一行 
 		{
-			s = 0;
-			for (j = k + 1; j <= n - 1; j++)
+			temp = 0;
+			for (j = k + 1; j < n; j++)  //k确定某一行后，j遍历从该行第k+2列遍历到最后一列
 			{
-				s += a[k][j] * x[j];
+				temp += a[k][j] * x[j];  //将上一次运算(增广矩阵中的下一行)得到的部分解回代到当前的行
 			}
-			x[k] = (a[k][n] - s) / a[k][k];
+			x[k] = (a[k][n] - temp) / a[k][k];  //移项并用除法使未知数的系数变为1
 		}
 		printf("\n方程组的解:\n");
 		output(x, n);
