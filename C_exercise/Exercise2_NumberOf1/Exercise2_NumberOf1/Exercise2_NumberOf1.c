7d88e14ae0eabb88c53a086b91a72c2074cd3850
DDG-LAL <1935088714@qq.com>
2023-01-11 00:52:10 +0800

    Exercise2

---
+++ C_exercise/Exercise2_NumberOf1/Exercise2_NumberOf1/Exercise2_NumberOf1.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//求一个数在内存中的二进制序列中1的个数
+
+#include<stdio.h>
+
+int Number1(int a)
+{
+
+}
+
+int Number2(int a)
+{
+
+}
+
+int Number3(int a)
+{
+
+}
+
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	printf("%d %d %d", Number1(a), Number2(a), Number3(a));
+	return 0;
+}
