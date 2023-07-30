f7c89f099a0f6066329f314f3eb4d92c59ce8afd
DDG-LAL <1935088714@qq.com>
2023-07-30 22:58:58 +0800

    .

--- C_exercise/Exercise5_SumOfTwoNumbers/Exercise5_SumOfTwoNumbers/Exercise5_SumOfTwoNumbers.c
+++ C_exercise/Exercise5_SumOfTwoNumbers/Exercise5_SumOfTwoNumbers/Exercise5_SumOfTwoNumbers.c
@@ -6,33 +6,80 @@
 //可以按任意顺序返回答案。
 
 #include<stdio.h>
+#include<stdlib.h>
+//#include"uthash.h"
 
-int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+//法1：双层循环直接遍历
+int* twoSum1(int* nums, int numsSize, int target, int* returnSize)
 {
-    int* ret = (int*)malloc(2 * sizeof(int));  //定义一个储存下标值的指针
     for (int i = 0; i < numsSize; i++)
     {
         for (int j = i + 1; j < numsSize; j++)
         {
-            if (nums[i] + nums[j] == target)  //找到两个数字的和等于目标值
+            if (nums[i] + nums[j] == target)  //发现两个数字的和等于目标值
             {
+                int* ret = (int*)malloc(2 * sizeof(int));  //定义一个储存下标值的指针
                 ret[0] = i;  //将下标值存储到指针中
                 ret[1] = j;
-                *returnSize = 2;  //返回的指针长度应该是2
+                *returnSize = 2;  //返回长度2
                 return ret;  //返回储存下标值的指针
-            }//returnSize的值是给外部接口用的，用来告知返回数组的长度
+            }                //returnSize的值是给外部接口用的，用来告知返回数组的长度
         }
     }
+    *returnSize = 0;  //返回长度0
     return NULL; // 如果找不到两个数字的和等于目标值，返回空指针
 }
 
+//法2：哈希表
+//struct hashTable {
+//    int key;
+//    int val;
+//    UT_hash_handle hh;
+//};
+//
+//struct hashTable* hashtable;
+//
+//struct hashTable* find(int ikey) {
+//    struct hashTable* tmp;
+//    HASH_FIND_INT(hashtable, &ikey, tmp);
+//    return tmp;
+//}
+//
+//void insert(int ikey, int ival) {
+//    struct hashTable* it = find(ikey);
+//    if (it == NULL) {
+//        struct hashTable* tmp = malloc(sizeof(struct hashTable));
+//        tmp->key = ikey, tmp->val = ival;
+//        HASH_ADD_INT(hashtable, key, tmp);
+//    }
+//    else {
+//        it->val = ival;
+//    }
+//}
+//
+//int* twoSum2(int* nums, int numsSize, int target, int* returnSize) {
+//    hashtable = NULL;
+//    for (int i = 0; i < numsSize; i++) {
+//        struct hashTable* it = find(target - nums[i]);
+//        if (it != NULL) {
+//            int* ret = malloc(sizeof(int) * 2);
+//            ret[0] = it->val, ret[1] = i;
+//            *returnSize = 2;
+//            return ret;
+//        }
+//        insert(nums[i], i);
+//    }
+//    *returnSize = 0;
+//    return NULL;
+//}
 
 int main()
 {
     int a[] = { 8, 2, 3, 4, 9 };
-    int target = 5;
-    int rsize = 0;
-    int* r = twoSum(a, sizeof(a) / sizeof(a[0]), 6, &rsize);
-    printf("%d %d", r[0], r[1]);
+    int rsize1 = 0, rsize2 = 0;
+    int* r1 = twoSum1(a, sizeof(a) / sizeof(a[0]), 6, &rsize1);
+    //int* r2 = twoSum2(a, sizeof(a) / sizeof(a[0]), 6, &rsize2);
+    printf("%d %d", r1[0], r1[1]);
+    //printf("%d %d", r2[0], r2[1]);
     return 0;
 }
