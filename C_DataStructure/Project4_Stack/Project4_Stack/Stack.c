515a898e554843bb665ac2223beaf43f775a8984
DDG-LAL <1935088714@qq.com>
2026-01-07 15:28:55 +0800

    stack

--- C_DataStructure/Project4_Stack/Project4_Stack/Stack.c
+++ C_DataStructure/Project4_Stack/Project4_Stack/Stack.c
@@ -4,6 +4,20 @@
 
 int main()
 {
-
+	ST s;
+	ST* ps = &s;
+	STinit(ps);
+	STpush(ps, 9);
+	STpush(ps, 6);
+	STpush(ps, 3);
+	STpush(ps, 1);
+	printf("%d\n\n", STsize(ps));
+	while (!STempty(ps))
+	{
+		printf("%d ", STtop(ps));
+		STpop(ps);
+	}
+	printf("\n");
+	STdestroy(ps);
 	return 0;
 }
