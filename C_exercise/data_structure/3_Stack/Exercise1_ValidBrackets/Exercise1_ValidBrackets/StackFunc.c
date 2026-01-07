bf7dcf783af4670857abec6d9d8f8f8d707daff6
DDG-LAL <1935088714@qq.com>
2026-01-07 16:31:43 +0800

    exercise

---
+++ C_exercise/data_structure/3_Stack/Exercise1_ValidBrackets/Exercise1_ValidBrackets/StackFunc.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"StackFunc.h"
+
+void STinit(ST* pst) //初始化
+{
+	assert(pst);
+	pst->data = NULL;
+	pst->cap = pst->top = 0;
+}
+
+bool STempty(ST* pst) //判断栈是否为空
+{
+	assert(pst);
+	return 0 == pst->top;
+}
+
+void STpush(ST* pst, STdatatype x) //入栈
+{
+	assert(pst);
+	if (pst->top == pst->cap)
+	{
+		int newcap = pst->cap == 0 ? 4 : pst->cap * 2;
+		STdatatype* tmp = (STdatatype*)realloc(pst->data, sizeof(STdatatype) * newcap);
+		if (!tmp)
+		{
+			perror("realloc\n");
+			return;
+		}
+		pst->data = tmp;
+		pst->cap = newcap;
+	}
+	pst->data[pst->top] = x;
+	pst->top++;
+}
+
+void STpop(ST* pst) //出栈
+{
+	assert(pst);
+	assert(!STempty(pst));
+	pst->top--;
+}
+
+STdatatype STtop(ST* pst) //获取栈顶元素
+{
+	assert(pst);
+	assert(!STempty(pst));
+	return pst->data[pst->top - 1];
+}
+
+int STsize(ST* pst) //获取栈内元素个数
+{
+	assert(pst);
+	return pst->top;
+}
+
+void STdestroy(ST* pst) //销毁
+{
+	assert(pst);
+	free(pst->data);
+	pst->data = NULL;
+	pst->cap = pst->top = 0;
+}
---
+++ C_exercise/data_structure/3_Stack/Exercise1_ValidBrackets/Exercise1_ValidBrackets/ValidBrackets.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//【判断有效括号】
+//给出一个只包括'('、')'、'{'、'}'、'['、']'的字符串s，判断字符串是否有效
+//有效字符串需满足：
+//1、左括号必须用相同类型的右括号闭合
+//2、左括号必须以正确的顺序闭合
+//3、每个右括号都有一个对应的相同类型的左括号
+//示例：
+//输入：s = "()[]{}({})"
+//输出：true
+//输入：s = "([)]"
+//输出：false
+
+#include"StackFunc.h"
+#include<string.h>
+
+bool isValid(char* s)
+{
+	ST stmp;
+	ST* ps = &stmp;
+	STinit(ps);
+	while (*s)
+	{
+		if (*s == '(' || *s == '[' || *s == '{')
+		{
+			STpush(ps, *s);
+			s++;
+		}
+		if (*s == ')' || *s == ']' || *s == '}')
+		{
+			if (STempty(ps))
+				return false;
+			if (((STtop(ps) == '(') && (*s == ')')) ||
+				((STtop(ps) == '[') && (*s == ']')) ||
+				((STtop(ps) == '{') && (*s == '}')))
+			{
+				STpop(ps);
+				s++;
+			}
+			else
+				return false;
+		}
+	}
+	return STempty(ps);
+}
+
+void test(char* s)
+{
+	if (isValid(s)) printf("true\n\n");
+	else printf("false\n\n");
+}
+
+int main()
+{
+	char s1[] = "()[]{}({})";
+	char s2[] = "([)]";
+	char s3[] = "]";
+	char s4[] = "[[]";
+
+	test(s1);
+	test(s2);
+	test(s3);
+	test(s4);
+
+	return 0;
+}
