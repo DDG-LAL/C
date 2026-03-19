b9ce1b847678a9e79923efcf641b27cc50485b5f
DDG-LAL <1935088714@qq.com>
2026-03-19 11:22:44 +0800

    .

--- C_exercise/basic/Exercise11_MyOffsetof/Exercise11_MyOffsetof/Exercise11_MyOffsetof.c
+++ C_exercise/basic/Exercise11_MyOffsetof/Exercise11_MyOffsetof/Exercise11_MyOffsetof.c
@@ -3,6 +3,9 @@
 //用宏实现计算结构体中某成员变量地址相对于首地址的偏移量
 
 #define MY_OFFSETOF(type,memname) (size_t)&(((type*)0)->memname)  //以0为首地址，则取到的成员变量地址的值即等于偏移量
+// ((type*)0): 将0强转为type*类型
+// &(((type*)0)->memname): 将0视为type类型的结构体，对该结构体的memname成员取地址
+// (size_t)&(((type*)0)->memname): 强转为size_t类型，此时取到的成员变量地址即为偏移量
 #include<stdio.h>
 #include<stddef.h>
 
